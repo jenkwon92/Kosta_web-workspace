bf6cbb594a2eeff6dcedb976684a1272aff72eb3
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-09-15 16:29:37 +0900

    210915

---
+++ webstudy04-javascript/src/main/webapp/js/step3.js
@@ -0,0 +1,15 @@
+/**
+ * step3-function.html에서 사용하는 함수를 정의
+ */
+function hello() {
+	console.log("hello 함수실행");
+	alert("hello 함수실행 맛점하세요");
+}
+function bye(message) {
+	alert(message);
+}
+function testParam(info, num) {
+	alert(info + " " + num);
+}
+
+
