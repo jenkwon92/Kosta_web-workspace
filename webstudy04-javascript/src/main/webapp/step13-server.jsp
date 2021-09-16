802166718d9812f181905c7a0aefdf460afd3c9a
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-09-16 17:01:58 +0900

    210916

---
+++ webstudy04-javascript/src/main/webapp/step13-server.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>server jsp</title>
+</head>
+<body>
+<!-- jsp는 지금 수업하는 내용은 아닙니다 
+	step13-submit.html의 form에서 submit했을 때 서버가 응답한다는 것을 단지 확인하기 위한 것이고 이후에 
+	자세히 수업할 예정입니다.
+-->
+서버에서 정보를 전달받아 응답한다 <br><br>
+별명 :<%=request.getParameter("nick") %> 나이: <%=request.getParameter("age") %>
+</body>
+</html>
---
+++ webstudy05-servlet-basic/src/main/java/step1/HelloServlet.java
@@ -0,0 +1,40 @@
+package step1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+ // reloadable ="true" 해두었기때문에 저장하자마자 컨텍스트를 다시 로드함
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h3>Hello Servlet 즐연휴</h3>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+
+}
