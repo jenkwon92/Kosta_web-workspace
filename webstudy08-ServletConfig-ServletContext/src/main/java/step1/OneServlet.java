4c1f12cc3a7fc98b4d321922c50181b5e7312c41
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-09-27 11:45:47 +0900

    210927

---
+++ webstudy08-ServletConfig-ServletContext/src/main/java/step1/OneServlet.java
@@ -0,0 +1,46 @@
+package step1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OneServlet
+ */
+public class OneServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OneServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 *ServletConfig와 ServletContext를 테스트하는 예제
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		ServletConfig config = getServletConfig(); 
+		//ServletConfig로부터 현 서블릿의 이름을 받아온다
+		out.println("<h3>"+config.getServletName()+"</h3>");
+		String springConfig = config.getInitParameter("contextConfigLocation");
+		out.println("<h4>"+springConfig+" 스프링 설정파일을 로딩</h4>");
+		//웹 어플리케이션 공유 자원인 ServletContext 객체에 접근하여
+		//특정 정보를 할당해본다
+		ServletContext context = config.getServletContext();
+		context.setAttribute("notice","OneServlet이 할당한 정보");
+		out.println("<a href=TwoServlet> TwoServlet 에서 확인</a>");
+		out.close();
+	}
+
+}
