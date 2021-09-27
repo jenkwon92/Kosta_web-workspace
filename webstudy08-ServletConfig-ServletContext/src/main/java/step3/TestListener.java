ab02aca636f472c860a1d9c9aec4d576f7e602ab
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-09-27 12:36:59 +0900

    210927

---
+++ webstudy08-ServletConfig-ServletContext/src/main/java/step3/TestListener.java
@@ -0,0 +1,33 @@
+package step3;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Application Lifecycle Listener implementation class TestListener
+ *
+ */
+public class TestListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public TestListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
